0dfc99877dd6d7bfc99cad8e802c7225cf617be0
Jochem Smit <jhsmit@gmail.com>
2023-04-05 18:29:04 +0200

    with global state

--- examples/listitems_globalstate.py
+++ examples/listitems_globalstate.py
@@ -33,18 +33,26 @@ class BooleanList():
         self.bools.value = new_bools
 
 
-bools = BooleanList(Reactive([False] * len(FILE_LIST)))
-
-
 @dataclasses.dataclass
 class GlobalState():
-    bools: BooleanList = dataclasses.field(default=bools)
+    bools: BooleanList = dataclasses.field(default=BooleanList())
+    file_list: Reactive[List[dict]] = dataclasses.field(default_factory=reactive_factory(dict))
+
+    def delete_file_item(self, idx):
+        new_items = self.file_list.value.copy()
+        new_items.pop(idx)
+        self.file_list.value = new_items
+        self.bools.delete(idx)
+
+
+bools = BooleanList(Reactive([False] * len(FILE_LIST)))
+GLOBAL_STATE = GlobalState(bools=bools, file_list=Reactive(FILE_LIST))
 
 
 @solara.component
-def ListFileItem(file: dict, idx: int, on_delete: Callable[[int], None]):
+def ListFileItem(file: dict, idx: int):
     def on_toggle(value):
-        bools.toggle(idx)
+        GLOBAL_STATE.bools.toggle(idx)
 
     with rv.ListItem() as main:
         rv.ListItemIcon(left=True, children=[rv.Icon(children=[file['icon']])])
@@ -52,25 +60,17 @@ def ListFileItem(file: dict, idx: int, on_delete: Callable[[int], None]):
         rv.ListItemSubtitle(children=[file["subtitle"]])
         with rv.Btn(icon=True) as button_delete:
             rv.Icon(children=["mdi-delete"])
-        rv.use_event(button_delete, "click", lambda *ignore_events: on_delete(idx))
+        rv.use_event(button_delete, "click", lambda *ignore_events: GLOBAL_STATE.delete_file_item(idx))
         with rv.ListItemAction() as action:
-            solara.Checkbox(value=bools.get(idx), on_value=on_toggle)
+            solara.Checkbox(value=GLOBAL_STATE.bools.get(idx), on_value=on_toggle)
 
     return main
 
 
 @solara.component
 def Page():
-    file_list, set_file_list = solara.use_state(FILE_LIST)
-
-    def on_delete(idx):
-        print(idx)
-        new_items = file_list.copy()
-        new_items.pop(idx)
-        set_file_list(new_items)
-        bools.delete(idx)
 
     with rv.List(max_width=500) as main:
-        for idx, file in enumerate(file_list):
-            ListFileItem(file, idx, on_delete)
+        for idx, file in enumerate(GLOBAL_STATE.file_list.value):
+            ListFileItem(file, idx)
 
