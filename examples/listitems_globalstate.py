dd187defb475169c5d447b99c774013f75dd1e45
Jochem Smit <jhsmit@gmail.com>
2023-04-05 18:30:30 +0200

    refactoring

--- examples/listitems_globalstate.py
+++ examples/listitems_globalstate.py
@@ -1,6 +1,5 @@
 import dataclasses
-from functools import partial
-from typing import List, Callable
+from typing import List
 
 import solara
 from solara.alias import rv
@@ -35,24 +34,24 @@ class BooleanList():
 
 @dataclasses.dataclass
 class GlobalState():
-    bools: BooleanList = dataclasses.field(default=BooleanList())
+    selected: BooleanList = dataclasses.field(default=BooleanList())
     file_list: Reactive[List[dict]] = dataclasses.field(default_factory=reactive_factory(dict))
 
     def delete_file_item(self, idx):
         new_items = self.file_list.value.copy()
         new_items.pop(idx)
         self.file_list.value = new_items
-        self.bools.delete(idx)
+        self.selected.delete(idx)
 
 
-bools = BooleanList(Reactive([False] * len(FILE_LIST)))
-GLOBAL_STATE = GlobalState(bools=bools, file_list=Reactive(FILE_LIST))
+selected = BooleanList(Reactive([False] * len(FILE_LIST)))
+GLOBAL_STATE = GlobalState(selected=selected, file_list=Reactive(FILE_LIST))
 
 
 @solara.component
 def ListFileItem(file: dict, idx: int):
     def on_toggle(value):
-        GLOBAL_STATE.bools.toggle(idx)
+        GLOBAL_STATE.selected.toggle(idx)
 
     with rv.ListItem() as main:
         rv.ListItemIcon(left=True, children=[rv.Icon(children=[file['icon']])])
@@ -62,14 +61,12 @@ def ListFileItem(file: dict, idx: int):
             rv.Icon(children=["mdi-delete"])
         rv.use_event(button_delete, "click", lambda *ignore_events: GLOBAL_STATE.delete_file_item(idx))
         with rv.ListItemAction() as action:
-            solara.Checkbox(value=GLOBAL_STATE.bools.get(idx), on_value=on_toggle)
+            solara.Checkbox(value=GLOBAL_STATE.selected.get(idx), on_value=on_toggle)
 
     return main
 
-
 @solara.component
 def Page():
-
     with rv.List(max_width=500) as main:
         for idx, file in enumerate(GLOBAL_STATE.file_list.value):
             ListFileItem(file, idx)
