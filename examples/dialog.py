a752aebfa4949d46617195fef5478564e22600d4
Jochem Smit <jhsmit@gmail.com>
2023-05-15 11:07:50 +0200

    dialog

--- examples/dialog.py
+++ examples/dialog.py
@@ -6,10 +6,12 @@ from solara.alias import rv
 def Page():
     show_code, set_show_code = solara.use_state(False)
     print('show', show_code)
-    solara.Button("Open dialog", on_click=lambda *args: set_show_code(True))
 
-    with rv.Dialog(v_model=show_code, on_v_model=set_show_code) as diag:
-        with rv.Sheet():
-            solara.Info("Hello world")
-            solara.Button("Close dialog", on_click=lambda *args: set_show_code(False))
+    with rv.Sheet() as main:
+        solara.Button("Open dialog", on_click=lambda: set_show_code(True))
+
+        with rv.Dialog(v_model=show_code, on_v_model=set_show_code):
+            with rv.Sheet():
+                solara.Info("Hello world")
+                solara.Button("Close dialog", on_click=lambda *args: set_show_code(False))
 
