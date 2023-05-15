46edb79339788ff8c28519acd5da87448ad8f624
Jochem Smit <jhsmit@gmail.com>
2023-05-15 14:07:08 +0200

    input form

--- examples/input_form.py
+++ examples/input_form.py
@@ -30,10 +30,10 @@ def ValidationForm(value: Union[BaseModel, Reactive[BaseModel]], on_value: Optio
                 error_value.update(**{name: []})
         except ValidationError as e:
             errors = e.errors()
-            if len(errors) > 1:
-                raise ValueError("Can only handle one error") from e
-            error = errors[0]
-            error_value.update(**{name: [error['msg']]})
+            for err in errors:
+                if len(err['loc']) > 1:
+                    raise ValueError("Sub-models are not supported") from e
+                error_value.update(**{err['loc'][0]: [err['msg']]})
 
     layout_cm = layout or solara.Column
     with layout_cm():
@@ -52,8 +52,6 @@ def ValidationForm(value: Union[BaseModel, Reactive[BaseModel]], on_value: Optio
             with cm:
                 rv.TextField(**textfield_kwargs)
 
-    solara.Button(label='print', on_click=lambda: print(model_value.value))
-
 
 def humanize_snake_case(s: str) -> str:
     # the human already thought about humanization or does not stick to snake_case
@@ -67,14 +65,16 @@ class ExperimentMetadata(BaseModel):
 
     experiment_label: Annotated[str, Field(description="Label for the experiment.", max_length=10)] = "Experiment 1"
 
-    pH: Annotated[float, Field(le=14, ge=2., description="Uncorrected pH as read with a pH meter")] = 7.0
+    pH: Annotated[float, Field(le=14, ge=2., description="pH value of reaction buffer.")] = 7.0
 
-    temperature: Annotated[float, Field(ge=0., le=100,  title="Temperature (C)", description="Temperature reaction mixture.")] = 25.0
+    temperature: Annotated[float, Field(ge=0., le=100,  title="Reaction temperature", description="Reaction mixture temperature in Celsius.")] = 25.0
 
 
 @solara.component
 def Page():
-    global_exp_metadata = solara.use_reactive(ExperimentMetadata())
+    exp_metadata = solara.use_reactive(ExperimentMetadata())
 
     field_options = {'temperature': {'suffix': '\u2103'}}
-    ValidationForm(global_exp_metadata, field_options=field_options)
+    ValidationForm(exp_metadata, field_options=field_options, layout=partial(solara.Column, classes=['ma-4']))
+    
+    solara.Button(label='print', on_click=lambda: print(exp_metadata.value))
